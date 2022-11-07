2b25d2e563d993f9fa5d721aa38328882c5f5922
Command_String <bmxmikey2@gmail.com>
2022-11-06 23:47:32 -0500

    Removed code that wasn't being used

--- src/Interfaces/HandlerInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CommandString\Router\Abstract;
-
-use Psr\Http\Message\ResponseInterface;
-
-interface HandlerInterface {
-    public function handle(ResponseInterface $response): ResponseInterface;
-}
